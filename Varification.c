1c0bac5b8d2c84d9f4b69cd861fd561e4052c076
Rakshicoder <obalchandra472@gmail.com>
2025-11-21 15:18:50 +0530

    Create Varification.c

---
+++ Varification.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int choice,id;
+    printf("1.PAN Number\n");
+    printf("2.AADHAR Number\n");
+    printf("3.APARR Id\n");
+    printf("4.DRIVING Licence\n");
+    printf("5.Passport\n");
+    printf("Enter your choice (1-5)");
+    scanf("%d",& choice);
+    {
+        case 1:
+        printf("PAN number\n");
+        break;
+        case 2:
+        printf("AADHAR number\n");
+        break;
+        case 3:
+        printf("APARR number\n");
+        break;
+        case 4:
+        printf("DRIVING Licence \n");
+        case 5:
+        printf("Passport\n");
+        break;
+        
+        defalt:
+        printf("no varified");
+        
+        
+    }
