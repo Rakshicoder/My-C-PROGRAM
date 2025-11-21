457144d4a4b308b89bb5b2c2264c12e62c637692
Rakshicoder <obalchandra472@gmail.com>
2025-11-21 10:12:39 +0530

    Create Mallic free.c

---
+++ Mallic free.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *p=(int*) malloc(sizeof(int));
+    *p=10;
+    
+    free(p);
+    printf("value=%p\n",*p);
+     return 0;
+    
+}   
